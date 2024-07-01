881c547185d2d244b0900353a1c1d6030423adea
Mohamed Gamal <MohamedGamal.sde@gmail.com>
2024-07-01 21:22:31 +0300

    Fix and migrate  tailwind import-export back to ES module

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+import animatePlugin from 'tailwindcss-animate';
+
+export default {
   darkMode: ['class'],
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
   prefix: '',
@@ -68,5 +71,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [animatePlugin],
 };
